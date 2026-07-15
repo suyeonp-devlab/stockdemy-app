ae20c18ce259d7651706ba6d20ada8c7bf603004
suyeon <suyeondevlab@gmail.com>
2026-07-16 00:00:18 +0900

    style: input 컴포넌트 스타일 수정

--- src/shared/components/form/Input.tsx
+++ src/shared/components/form/Input.tsx
@@ -8,11 +8,16 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 export default function Input({
   error,
   className,
+  spellCheck = false,
   ...props
 }: InputProps) {
 
   return (
     <input
+      spellCheck={spellCheck}
+      data-gramm="false"
+      data-gramm_editor="false"
+      data-enable-grammarly="false"
       className={clsx(
         "w-full px-4 py-3 rounded-md border bg-gray-800 text-sm text-gray-100 placeholder-gray-600 focus:outline-none transition-colors",
         error ? "border-red-500 focus:border-red-400" : "border-gray-700 focus:border-blue-400",
