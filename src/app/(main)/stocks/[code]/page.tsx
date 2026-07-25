62b40feafce082d33b5eb491f6873b97e4761810
suyeon <suyeondevlab@gmail.com>
2026-07-25 22:18:57 +0900

    wip: 종목 상세 페에지 작업

--- src/app/(main)/stocks/[code]/page.tsx
+++
@@ -1,6 +0,0 @@
-import StockDetailPage from "@/features/stocks/StockDetailPage";
-
-export default async function StockDetailRoutePage({ params }: { params: Promise<{ code: string }> }) {
-  const { code } = await params;
-  return <StockDetailPage code={code} />;
-}
